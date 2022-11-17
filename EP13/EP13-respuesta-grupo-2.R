6be41009cd090b0025b174470c2d1add90f6b3a5
Gabriela Fernández R <gabriela.fernandez.r@usach.cl>
2022-11-17 11:41:09 -0300

    inicio 13

---
+++ EP13/EP13-respuesta-grupo-2.R
@@ -0,0 +1,26 @@
+# Ejercicio Práctico N°13
+# Grupo N°2
+# Integrantes:
+#   Ivan Zuñiga Quiroz
+#   Bastian Soto Jaña
+#   Gabriela Fernández Rodríguez
+#   Nicolás Gabrielli
+
+# Librerias importadas.
+
+library(ez)
+library(ggpubr)
+library(tidyr)
+library(dplyr)
+library(boot)
+library(simpleboot)
+library(bootES)
+library(WRS2)
+library(stringr)
+
+
+
+#-------------------------- Enunciado:  ----------------------------------------
+
+  
+  

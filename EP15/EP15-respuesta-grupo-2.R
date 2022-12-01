dac92fc048b35d9a8dd4fb653af366456eb9d9cd
Ivan Zuñiga Quiroz <ivan.zuniga@usach.cl>
2022-12-01 15:35:07 -0300

    comentarios de los vifs

--- EP15/EP15-respuesta-grupo-2.R
+++ EP15/EP15-respuesta-grupo-2.R
@@ -119,7 +119,9 @@ muestra_datos<- rbind(muestra_sobrepeso,muestra_nosobrepeso)
 #    luego utilizar las funciones del paquete caret para construir un modelo de regresión
 #    lineal múltiple con los predictores escogidos y evaluarlo usando bootstrapping.
 
+# Se crea la variable borrar, para eliminar las variables que no se deben utilizar
 borrar <-c("EN","IMC")
+
 muestra_sin_EN_IMC <- muestra_datos[,!(names(muestra_datos) %in% borrar)]
 nombre.variables <- colnames(muestra_sin_EN_IMC)
 
@@ -135,23 +137,23 @@ print(plot(modelos))
 # Viendo la matriz obtenida, elegimos "Chest.depth", "Chest.diameter",
 # "Waist.Girth", "Hip.Girth", "Forearm.Girth", "Calf.Maximum.Girth",
 # "Age" y "Height"
-
 seleccionados_mas_peso <- c("Chest.depth", "Chest.diameter","Waist.Girth", "Hip.Girth", 
                    "Forearm.Girth", "Calf.Maximum.Girth", "Age", "Height","Weight")
 muestra_datos_modelo <- muestra_sin_EN_IMC %>%select(seleccionados_mas_peso)
 
-# Uso de caret    
+# Usamo caret para ajustar el modelo.    
 
 # Ajustar modelo usando validación cruzada de 5 pliegues.
 modelo <- train(Weight ~ ., data = muestra_datos_modelo, method = "lm",
                 trControl = trainControl(method = "boot", number = 5))
 print(summary(modelo))
 
+
 vifs <- vif(modelo$finalModel)
 print(vifs)
 
+# Modificamos el predictor eliminando la variable "waist.Girth"
 muestra_datos_modelo_p1 <- muestra_datos_modelo %>% select(!"Waist.Girth")
-
 # Ajustar modelo usando validación cruzada de 5 pliegues.
 modelo_p1 <- train(Weight ~ ., data = muestra_datos_modelo_p1, method = "lm",
                 trControl = trainControl(method = "boot", number = 5))
@@ -160,7 +162,7 @@ print(summary(modelo_p1))
 vifs_p1 <- vif(modelo_p1$finalModel)
 print(vifs_p1)
 
-
+# Modificamos el predictor eliminando la variable "Age"
 muestra_datos_modelo_p2 <- muestra_datos_modelo_p1 %>% select(!"Age")
 
 # Ajustar modelo usando validación cruzada de 5 pliegues.
@@ -171,6 +173,7 @@ print(summary(modelo_p2))
 vifs_p2 <- vif(modelo_p2$finalModel)
 print(vifs_p2)
 
+# Modificamos el predictor eliminando la variable "Forearm.Girth"
 muestra_datos_modelo_p3 <- muestra_datos_modelo_p2 %>% select(!"Forearm.Girth")
 
 # Ajustar modelo usando validación cruzada de 5 pliegues.
@@ -181,6 +184,7 @@ print(summary(modelo_p3))
 vifs_p3 <- vif(modelo_p3$finalModel)
 print(vifs_p3)
 
+# Modificamos el predictor eliminando la variable "Chest.diameter"
 muestra_datos_modelo_p4 <- muestra_datos_modelo_p3 %>% select(!"Chest.diameter")
 # Ajustar modelo usando validación cruzada de 5 pliegues.
 modelo_p4 <- train(Weight ~ ., data = muestra_datos_modelo_p4, method = "lm",
